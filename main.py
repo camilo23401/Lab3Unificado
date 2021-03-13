d82eb7dc6f83d82626ed2e1ae95fa495dd7d8363
Infracom <lh.cabra@uniandes.edu.co>
2021-03-13 22:46:37 +0000

    Log del cliente

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 #coding=utf-8
 import socket
 import os
 import hashlib
 from datetime import datetime
 import threading
-
+from time import time
 
 def conexion():
     host = '127.0.0.1'
@@ -26,23 +27,23 @@ def conexion():
     min = datetime.now().minute
     seg = datetime.now().second
 
-#    fecha= "/"+str(year)+"-"+str(mes)+"-"+str(dia)+"-"+str(hora)+"-"+str(min)+"-"+str(seg)+"log"
-#    print(fecha)
-#    archivolog = path_logs+fecha+".txt"
-#    file = open(archivolog, "a")
+    fecha= "/"+str(year)+"-"+str(mes)+"-"+str(dia)+"-"+str(hora)+"-"+str(min)+"-"+str(seg)+"log"
+    print(fecha)
+    archivolog = path_logs+fecha+".txt"
+    file = open(archivolog, "w")
 
     cliente= threading.currentThread().getName()
     print("Cliente "+cliente)
     print("Conectado al servidor")
-
+    start_time=time()
+    cant_paquetes=0
+    peso_tot=0
     while (True):
         mens = "Mensaje de confirmacion. Listo para recibir un archivo"
         obj.send(mens)
         recibido = obj.recv(4096)
         print("Se recibio un archivo del servidor")
-#        log = "nombre del archivo: "
-#        log=log+" Tamaño: "+os.stat(recibido).st_size
-#        log = log+"\n Cliente: "+cliente
+        file.write("\n Cliente: "+cliente)
 # ------------------------------------- HASHING! ------------------------------------------------
 #        print("En espera de hash por parte del servidor")
 #        hash = obj.recv(4096)
@@ -60,17 +61,25 @@ def conexion():
         file1 = open(archivoPorEscribir, "wb")
         file1.write(recibido)
         file1.close()
-#        log = log+"\n Entrega exitosa: "
-#        log=log+"\n Tiempo de transferencia: "
-#        log=log+"\n Número de paquetes recibidos: "+" número de bytes recibidos: "
-#        file.write(log)
-#        file.close()
+	file.write("\n El archivo recibido fue : "+nombre_archivo)
+	file.write("\n El peso fue de :"+str(os.path.getsize(archivoPorEscribir))+" Bytes")
+        cant_paquetes=cant_paquetes+1
+        peso_tot=peso_tot+float(os.path.getsize(archivoPorEscribir))
+        file.write("\n Entrega exitosa: ")
+	if(os.path.getsize(archivoPorEscribir)!=0):
+		file.write("si")
+	else:
+		file.write("no")
         break
     obj.close()
+    file.write("\n Cantidad de paquetes recibidos: "+str(cant_paquetes))
+    file.write("\n total bytes recibidos: "+str(peso_tot))
+    file.write("\n Tiempo de transferencia: "+str(time()-start_time)+"seg")
+    file.close()
     print("Conexión cerrada")
 
 
-NUM_HILOS = 1
+NUM_HILOS = 25
 
 for num_hilo in range(NUM_HILOS):
     hilo = threading.Thread(target=conexion,name='Cliente'+str(num_hilo))
