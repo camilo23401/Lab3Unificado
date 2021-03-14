9386d1a1a3129624dd173c2cf3418d02107eee7f
nestorivan95 <ni.ramirez10@uniandes.edu.co>
2021-03-14 18:27:46 -0500

    Cambios servidor

--- Servidor.py
+++ Servidor.py
@@ -3,10 +3,11 @@
 import socket
 import threading
 import os
+import hashlib
 
 # Creamos el socket del servidor TCP:
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Socket Created")
+print ("Socket creado")
 
 # Se define el host
 host = "localhost"
@@ -16,7 +17,7 @@ port = 6666
 
 # Lo vinculamos al puerto con la función bind()
 sock.bind((host, port))
-print ("socket bind complete")
+print ("Socket bind completado con host " +host+" y puerto "+port)
 
 # Establecemos un *timeout*
 sock.settimeout(60)
@@ -29,21 +30,33 @@ sock.listen(1)
 while True:
     # Se establece la conexion con el cliente
     connection, client_address = sock.accept()
-    print ('Got connection from', client_address)
-    print ("Receiving...")
+    print ('Conexion obtenida de ', client_address)
+    print ("Recibiendo...")
     connection.recv(4096)
-    print("Recibio rta del cliente")
+    print("Recibio respuesta del cliente")
     directory_path = os.path.dirname(__file__)
-    ruta = os.path.join(directory_path,"ArchivosParaEnviar/Archivo100M.txt") 
+    ruta = os.path.join(directory_path,"ArchivosParaEnviar/Archivo100M.txt")
     data = open(ruta).read(4096)
+
+    try:
+        hash = hashlib.sha256()
+        with open(data, "rb") as f:
+            for bloque in iter(lambda: f.read(4096), b""):
+                hash.update(bloque)
+                hash.hexdigest()
+    except Exception as e:
+        print("Error: %s" % (e))
+    except:
+        print("Error desconocido")
+        
     connection.send(data)
-    print("Connection sent")
+    print("Conexion enviada")
     #if data == b"DONE":
     #    print("Done Receiving.")
     #    break
     #file.write(data)
 file.close()
-connection.send("End of connection")
+connection.send("Fin de la conexion")
 connection.shutdown(2)
 connection.close()
-sock.close()  
+sock.close()
