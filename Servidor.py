d721dbf2e0352bac11496c13f878b03101de1267
camilo23401 <ca.garciav@uniandes.edu.co>
2021-03-16 18:07:17 -0500

    Funcionando?

--- Servidor.py
+++ Servidor.py
@@ -20,7 +20,7 @@ def cambiarRuta(numArchivo):
     global tamano
     global nomArchivo
     if (numArchivo == "1"):
-        ruta = os.path.join(directory_path, "ArchivosParaEnviar/ArchivoPrueba.txt")
+        ruta = os.path.join(directory_path, "ArchivosParaEnviar/Archivo100M.txt")
         tamano = 100
         nomArchivo = "Archivo100M"
     if (numArchivo == "2"):
@@ -28,89 +28,100 @@ def cambiarRuta(numArchivo):
         tamano = 200
         nomArchivo = "Archivo250M"
 
-# Creamos el socket del servidor TCP:
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Socket creado")
+aux = 0
+def conexionServ():
+    global aux
+    # Creamos el socket del servidor TCP:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket creado")
 
-# Se define el host
-host = "localhost"
+    # Se define el host
+    host = "localhost"
 
-# Se define el puerto
-port = 6666
-path_log="./Logs"
-year=datetime.now().year
-mes=datetime.now().month
-dia=datetime.now().day
-hora=datetime.now().hour
-min=datetime.now().minute
-seg=datetime.now().second
+    # Se define el puerto
+    port = 6666+aux
+    aux +=1
+    path_log = "./Logs"
+    year = datetime.now().year
+    mes = datetime.now().month
+    dia = datetime.now().day
+    hora = datetime.now().hour
+    min = datetime.now().minute
+    seg = datetime.now().second
 
-fecha="/"+str(year)+"-"+str(mes)+"-"+str(dia)+"-"+str(hora)+"-"+str(min)+"-"+str(seg)+"log"
-archivolog=path_log+fecha+".txt"
-file=open(archivolog,"w")
+    fecha = "/" + str(year) + "-" + str(mes) + "-" + str(dia) + "-" + str(hora) + "-" + str(min) + "-" + str(
+        seg) + "log"
+    archivolog = path_log + fecha + ".txt"
+    file = open(archivolog, "w")
 
-# Lo vinculamos al puerto con la función bind()
-sock.bind((host, port))
-print ("Socket bind completado con host " +str(host)+" y puerto "+str(port))
+    # Lo vinculamos al puerto con la función bind()
+    sock.bind((host, port))
+    print("Socket bind completado con host " + str(host) + " y puerto " + str(port))
 
-# Establecemos un *timeout*
-sock.settimeout(60)
+    # Establecemos un *timeout*
+    sock.settimeout(60)
 
-# Ponemos el servidor en modo escucha:
-sock.listen(int(numCliente))
+    # Ponemos el servidor en modo escucha:
+    sock.listen(int(numCliente))
 
-cambiarRuta(archivo)
-numeroConectados = 0
-while True:
-    # Se establece la conexion con el cliente
-    connection, client_address = sock.accept()
-    print ('Conexion obtenida de ', client_address)
-    numeroConectados+=1
-    print ("Recibiendo solicitudes...")
-    print(numeroConectados);
-    connection.recv(4096)
-    print("Recibio respuesta del cliente")
+    cambiarRuta(archivo)
+    numeroConectados = 0
 
-    datax = open(ruta, encoding='utf-8')
+    while True & numeroConectados<=int(numCliente):
+        # Se establece la conexion con el cliente
+        connection, client_address = sock.accept()
+        print ('Conexion obtenida de ', client_address)
+        numeroConectados+=1
+        print ("Recibiendo solicitudes...")
+        print(numeroConectados);
+        connection.recv(4096)
+        print("Recibio respuesta del cliente")
 
-    hash = hashlib.sha256()
-    start_time=time()
-    cant_paquetes=0
-    peso_tot=0
-    fb = datax.read(65536)
-    while len(fb) > 0:
-        hash.update(fb.encode('utf-8'))
+        datax = open(ruta, encoding='utf-8')
+
+        hash = hashlib.sha256()
+        start_time=time()
+        cant_paquetes=0
+        peso_tot=0
         fb = datax.read(65536)
-    resultadoHash = hash.hexdigest()
-    strHas = str(resultadoHash).encode('utf-8')
-    connection.send(bytes(strHas))
-    print("Se envió el HASH al cliente")
+        while len(fb) > 0:
+            hash.update(fb.encode('utf-8'))
+            fb = datax.read(65536)
+        resultadoHash = hash.hexdigest()
+        strHas = str(resultadoHash).encode('utf-8')
+        connection.send(bytes(strHas))
+        print("Se envió el HASH al cliente")
 
-    data = open(ruta, encoding='utf-8')
-    dr = data.read(4096)
-    while len(dr) > 0:
-        connection.send(dr.encode('utf-8'))
-        print("Enviando paquete")
+        data = open(ruta, encoding='utf-8')
         dr = data.read(4096)
-    print("Todos los paquetes fueron enviados")
-    cant_paquetes=cant_paquetes+1
-    peso_tot=peso_tot+float(os.path.getsize(ruta))
-    file.write("\n El archivo enviado fue: "+ nomArchivo )
-    file.write("\n El tamaño del archivo es: "+ str(tamano)+"MB")
-    file.write("\n La entrega fue exitosa")
-    file.write("\n El cliente al que fue enviado el archivo: "+str(client_address))
-    file.write("\n El peso total transferido fue: "+str(peso_tot))
-    file.write("\n La cantidad de paquetes transferida fue: "+str(cant_paquetes))
-    file.write("\n El tiempo de transferencia fue: "+str(time()-start_time)+" seg")
-    connection.shutdown(socket.SHUT_WR)
+        while len(dr) > 0:
+            connection.send(dr.encode('utf-8'))
+            print("Enviando paquete")
+            dr = data.read(4096)
+        print("Todos los paquetes fueron enviados")
+        cant_paquetes=cant_paquetes+1
+        peso_tot=peso_tot+float(os.path.getsize(ruta))
+        file.write("\n El archivo enviado fue: "+ nomArchivo )
+        file.write("\n El tamaño del archivo es: "+ str(tamano)+"MB")
+        file.write("\n La entrega fue exitosa")
+        file.write("\n El cliente al que fue enviado el archivo: "+str(client_address))
+        file.write("\n El peso total transferido fue: "+str(peso_tot))
+        file.write("\n La cantidad de paquetes transferida fue: "+str(cant_paquetes))
+        file.write("\n El tiempo de transferencia fue: "+str(time()-start_time)+" seg")
+        connection.shutdown(socket.SHUT_WR)
+        file.close()
+        connection.shutdown(2)
+        connection.close()
+        sock.close()
+        break
 
     #print("Conexion enviada")
     #if data == b"DONE":
     #    print("Done Receiving.")
     #    break
     #file.write(data)
-file.close()
-connection.send("Fin de la conexion")
-connection.shutdown(2)
-connection.close()
-sock.close()
+
+for num_hilo in range(int(numCliente)):
+    hilo = threading.Thread(target=conexionServ,name='Servidor'+str(num_hilo))
+    hilo.start()
+
--- main.py
+++ main.py
@@ -9,96 +9,100 @@ import threading
 from time import time
 import sys
 
+aux = 0
 def conexion():
+    global aux
     host = '127.0.0.1'
-    port = 6666
+    port = 6666+aux
+    aux+=1
     path_archivos = "./ArchivosRecibidos"
     path_logs = "./Logs"
 
     BLOCK_SIZE = 65536
     conexiones = 25
     obj = socket.socket()
+    try:
+        obj.connect((host, port))
+        cliente = threading.currentThread().getName()
+        print("Cliente " + cliente + " conectado al servidor")
 
-    obj.connect((host, port))
-    cliente = threading.currentThread().getName()
-    print("Cliente " + cliente + " conectado al servidor")
+        year = datetime.now().year
+        mes = datetime.now().month
+        dia = datetime.now().day
+        hora = datetime.now().hour
+        min = datetime.now().minute
+        seg = datetime.now().second
 
-    year=datetime.now().year
-    mes = datetime.now().month
-    dia = datetime.now().day
-    hora= datetime.now().hour
-    min = datetime.now().minute
-    seg = datetime.now().second
+        fecha = "/" + str(year) + "-" + str(mes) + "-" + str(dia) + "-" + str(hora) + "-" + str(min) + "-" + str(
+            seg) + "log"
+        archivolog = path_logs + fecha + ".txt"
+        file = open(archivolog, "w")
 
-    fecha= "/"+str(year)+"-"+str(mes)+"-"+str(dia)+"-"+str(hora)+"-"+str(min)+"-"+str(seg)+"log"
-    archivolog = path_logs+fecha+".txt"
-    file = open(archivolog, "w")
-
-
-    start_time=time()
-    cant_paquetes=0
-    peso_tot=0
-    while (True):
-        obj.send(b"Mensaje de confirmacion. Listo para recibir un archivo")
-        serverHash = obj.recv(4096)
-        print("Recibió el hash del archivo del servidor")
-        nombre_archivo = cliente + "-Prueba-" + str(conexiones)
-        archivoPorEscribir = os.path.join(path_archivos, nombre_archivo)
-        file1 = open(archivoPorEscribir, "wb")
-        recibido = obj.recv(4096)
-        while len(recibido)>0:
-            print("Recibiendo paquete")
-            file1.write(recibido)
+        start_time = time()
+        cant_paquetes = 0
+        peso_tot = 0
+        while (True):
+            obj.send(b"Mensaje de confirmacion. Listo para recibir un archivo")
+            serverHash = obj.recv(97)
+            print("Recibió el hash del archivo del servidor")
+            nombre_archivo = cliente + "-Prueba-" + str(conexiones) +".txt"
+            archivoPorEscribir = os.path.join(path_archivos, nombre_archivo)
+            file1 = open(archivoPorEscribir, "wb")
             recibido = obj.recv(4096)
-        print("Terminó la recepción de paquetes. Se recibió un archivo completo")
-        file1.close()
-        dataHash = open(archivoPorEscribir)
-        file.write("\n El archivo recibido fue : "+nombre_archivo)
-        file.write("\n El peso fue de :"+str(os.path.getsize(archivoPorEscribir))+" Bytes")
-        cant_paquetes=cant_paquetes+1
-        peso_tot=peso_tot+float(os.path.getsize(archivoPorEscribir))
-        file.write("\n Entrega exitosa: ")
-        file.write("\n Cliente: "+cliente)
+            while len(recibido) > 0:
+                print("Recibiendo paquete")
+                file1.write(recibido)
+                recibido = obj.recv(4096)
+            print("Terminó la recepción de paquetes. Se recibió un archivo completo")
+            file1.close()
+            dataHash = open(archivoPorEscribir)
+            file.write("\n El archivo recibido fue : " + nombre_archivo)
+            file.write("\n El peso fue de :" + str(os.path.getsize(archivoPorEscribir)) + " Bytes")
+            cant_paquetes = cant_paquetes + 1
+            peso_tot = peso_tot + float(os.path.getsize(archivoPorEscribir))
+            file.write("\n Entrega exitosa: ")
+            file.write("\n Cliente: " + cliente)
 
-# ------------------------------------- HASHING! ------------------------------------------------
+            # ------------------------------------- HASHING! ------------------------------------------------
 
-        #print("En espera de hash por parte del servidor")
-        directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        ruta = os.path.join( directory_path, "ArchivosRecibidos/"+nombre_archivo)
-        data = open(ruta, encoding='utf-8')
-        print("Verificando integridad")
-        hash = hashlib.sha256()
-        fb = data.read(65536)
-        while len(fb) > 0:
-            hash.update(fb.encode('utf-8'))
+            # print("En espera de hash por parte del servidor")
+            directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+            ruta = os.path.join(directory_path, "ArchivosRecibidos/" + nombre_archivo)
+            data = open(ruta, encoding='utf-8')
+            print("Verificando integridad")
+            hash = hashlib.sha256()
             fb = data.read(65536)
-        resultadoHash = hash.hexdigest()
+            while len(fb) > 0:
+                hash.update(fb.encode('utf-8'))
+                fb = data.read(65536)
+            resultadoHash = hash.hexdigest()
+
+            if (resultadoHash == serverHash.decode('utf-8')):
+                print("Integridad confirmada")
+            else:
+                print("Falla de integridad")
+                print("Enviado por el servidor:")
+                print((serverHash.decode('utf-8')))
+                print("Recuperado por el cliente:")
+                print((resultadoHash))
+
+            if (os.path.getsize(archivoPorEscribir) != 0):
+                file.write("si")
+            else:
+                file.write("no")
+            break
+        obj.close()
+        file.write("\n Cantidad de paquetes recibidos: " + str(cant_paquetes))
+        file.write("\n total bytes recibidos: " + str(peso_tot))
+        file.write("\n Tiempo de transferencia: " + str(time() - start_time) + "seg")
+        file.close()
+        print("Conexión del cliente " + cliente + " cerrada")
+    except:
+        print("Conexión denegada. Se llegó al máximo permitido en el servidor")
 
 
-        if(resultadoHash==serverHash.decode('utf-8')):
-            print("Integridad confirmada")
-        else:
-            print("Falla de integridad")
-            print("Enviado por el servidor:")
-            print((serverHash.decode('utf-8')))
-            print("Recuperado por el cliente:")
-            print((resultadoHash))
 
-
-        if(os.path.getsize(archivoPorEscribir)!=0):
-            file.write("si")
-        else:
-            file.write("no")
-        break
-    obj.close()
-    file.write("\n Cantidad de paquetes recibidos: "+str(cant_paquetes))
-    file.write("\n total bytes recibidos: "+str(peso_tot))
-    file.write("\n Tiempo de transferencia: "+str(time()-start_time)+"seg")
-    file.close()
-    print("Conexión del cliente "+ cliente + " cerrada")
-
-
-NUM_HILOS = 20
+NUM_HILOS = 25
 
 for num_hilo in range(NUM_HILOS):
     hilo = threading.Thread(target=conexion,name='Cliente'+str(num_hilo))
